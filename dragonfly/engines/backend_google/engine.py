c63f8fb14e8a33ea73d92eb110d05d9f8016843f
James Stout <james.wolf.stout@gmail.com>
2018-12-02 19:06:56 -0800

    Make Google backend exit cleanly.

--- dragonfly/engines/backend_google/engine.py
+++ dragonfly/engines/backend_google/engine.py
@@ -20,7 +20,7 @@ try:
 except ImportError:
     pass
 
-from ... import get_accessibility_controller
+from ... import get_stopping_accessibility_controller
 from .dictation import GoogleSpeechDictationContainer
 from .timer import SimpleTimerManager
 from ..base import EngineBase
@@ -312,35 +312,36 @@ class GoogleSpeechEngine(EngineBase):
         )
 
         self.connect()
-        self._accessibility = get_accessibility_controller()
-        with MicrophoneStream(RATE, CHUNK) as stream:
-            while self._connected:
-                shutdown_event = threading.Event()
-                audio_generator = stream.generator(shutdown_event)
-                requests = (types.StreamingRecognizeRequest(audio_content=content)
-                            for content in audio_generator)
+        with get_stopping_accessibility_controller() as accessibility:
+            self._accessibility = accessibility
+            with MicrophoneStream(RATE, CHUNK) as stream:
+                while self._connected:
+                    shutdown_event = threading.Event()
+                    audio_generator = stream.generator(shutdown_event)
+                    requests = (types.StreamingRecognizeRequest(audio_content=content)
+                                for content in audio_generator)
 
-                # Add context phrases.
-                context_phrases = Counter()
-                for (_, grammar) in self._grammar_wrappers.items():
-                    # window = Window.get_foreground()
-                    # grammar.process_begin(window.executable, window.title,
-                    #                       window.handle)
-                    grammar.process_begin("", "", "")
-                    for rule in grammar._rules:
-                        if not rule.active or not rule.exported: continue
-                        context_phrases.update(rule.element.context_phrases())
-                config = streaming_config.config
-                del config.speech_contexts[:]
-                speech_context = config.speech_contexts.add()
-                self._log.debug("Total phrases: %s", len(context_phrases))
-                # The API currently restricts to max 500 phrases as of 11/4/2017.
-                speech_context.phrases.extend(
-                    sorted(w for w, c in context_phrases.most_common(500)))
-                # self._log.debug("Phrases: %s", speech_context.phrases)
+                    # Add context phrases.
+                    context_phrases = Counter()
+                    for (_, grammar) in self._grammar_wrappers.items():
+                        # window = Window.get_foreground()
+                        # grammar.process_begin(window.executable, window.title,
+                        #                       window.handle)
+                        grammar.process_begin("", "", "")
+                        for rule in grammar._rules:
+                            if not rule.active or not rule.exported: continue
+                            context_phrases.update(rule.element.context_phrases())
+                    config = streaming_config.config
+                    del config.speech_contexts[:]
+                    speech_context = config.speech_contexts.add()
+                    self._log.debug("Total phrases: %s", len(context_phrases))
+                    # The API currently restricts to max 500 phrases as of 11/4/2017.
+                    speech_context.phrases.extend(
+                        sorted(w for w, c in context_phrases.most_common(500)))
+                    # self._log.debug("Phrases: %s", speech_context.phrases)
 
-                responses = client.streaming_recognize(streaming_config, requests)
+                    responses = client.streaming_recognize(streaming_config, requests)
 
-                # Now, put the transcription responses to use.
-                if not self.process_responses(responses, shutdown_event):
-                    return
+                    # Now, put the transcription responses to use.
+                    if not self.process_responses(responses, shutdown_event):
+                        return
